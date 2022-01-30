852f2643d82d40ef2cbbd6a7a1afd16dace011a1
username <user@email.com>
2022-01-30 11:37:15 +0600

    first commit

---
+++ form.php
@@ -0,0 +1,34 @@
+<html>
+    <body>
+        <h2>Registration Form</h2>
+        
+            First Name:
+            <input type="text" name="fname"><br>
+            Last Name:
+            <input type="text" name="lname"><br>
+            Age:
+            <input type="number" age="age"><br>
+            Designation:
+            <input type="text" designation="designation"><br>
+            <input type="radio" designation="designation"> Junior Programmar
+            <input type="radio" designation="designation"> Senior Programmar
+            <input type="radio" designation="designation"> Project Lead <br>
+            Preferred Language:
+            <input type="text" language="language"><br>
+            <input type="radio" language="language"> Java
+            <input type="radio" language="language"> PHP
+            <input type="radio" language="language"> C++ <br>
+            Email:
+            <input type="text" email="email"><br>
+            Password:
+            <input type="password" pass="pass"><br>
+            Select Chose a file:
+            <input type="file" name="fileToUpload" id="fileToUpload"><br>
+            <input type="submit"> 
+            <input type="Reset">
+  
+
+</form>
+           
+        </body>
+</html>
