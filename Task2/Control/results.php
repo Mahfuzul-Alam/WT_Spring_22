017bac5040317a8870da944113c333bd096d7874
Mahfuzul-Alam <87558319+Mahfuzul-Alam@users.noreply.github.com>
2022-02-06 14:07:00 +0600

    Add files via upload

---
+++ Task2/Control/results.php
@@ -0,0 +1,80 @@
+<?php
+if(isset($_POST["Submit"])){
+    $fname=$_POST["fname"];
+    $lname=$_POST["lname"];
+    $age=$_POST["age"];
+    $pass=$_POST["pass"];
+    $email=$_POST["email"];
+
+if(isset($_POST["Submit"]))
+
+{
+    $fname= $_POST["fname"];
+    if ($fname=="")
+    {
+        echo "enter a valid name";
+    }
+    else
+    {
+        echo "First name is: ".$fname;
+    }
+    
+    $lname= $_POST["lname"];
+    echo "<br>";
+    if ($lname=="")
+    {
+        echo "enter a valid name";
+    }
+    else
+    {
+        echo "Last Name is: ".$lname;
+    }
+    echo "<br>";
+    if(empty($age)){
+        echo "Please enter a valid Age";
+    }
+    else{
+        echo "Age: ".$age;
+    }
+    echo "<br>";
+    if(empty($pass)){
+        echo "Please enter a password";
+    }
+    else if(strlen($pass)<8){
+        echo "Your password must be 8 or more characters";
+    }
+    else{
+        echo "Your Password is fine";
+    }
+    echo "<br>";
+    if(empty($email) || !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",$email))
+{
+    echo "you must enter email";
+}
+else{
+    echo "Email: ".$email;
+}
+echo "<br>";
+    if (isset($_POST["myradio"]))
+    {
+        echo "He is a ".$_POST["myradio"];
+    }
+    else
+    {
+        echo "Please select a Designation ";
+    }
+    echo "<br>";
+    if (isset($_POST["mycheckbox"]))
+    {
+        echo "Preferred Language ".$_POST["mycheckbox"];
+    }
+    else
+    {
+        echo "Please select a Language ";
+    }
+    
+}
+}
+
+
+?>
---
+++ Task2/View/rform.php
@@ -0,0 +1,38 @@
+<html>
+    <body>
+        <h2>Registration Form</h2>
+        <form action="" method="post">
+        
+            First Name:
+            <input type="text" name="fname"><br>
+            Last Name:
+            <input type="text" name="lname"><br>
+            Age:
+            <input type="number" name="age"><br>
+            Designation:
+            
+            <input type="radio" name="myradio" value ="Junior Programmar">Junior Programmar
+            <input type="radio" name="myradio" value= "Senior Programmar">Senior Programmar
+            <input type="radio" name="myradio" value= "Project Lead">Project Lead <br> 
+            Preferred Language:
+           
+            <input type="checkbox" name="mycheckbox" value="Java"> Java
+            <input type="checkbox" name="mycheckbox" value="PHP"> PHP
+            <input type="checkbox" name="mycheckbox" value=" C++"> C++ <br>
+            Email:
+            <input type="text" name="email"> <br>
+            Password:
+            <input type="password" name="pass"><br>
+            Select Chose a file:
+            <input type="file" name="fileToUpload" id="fileToUpload"><br>
+            <input type="submit" name="Submit">
+            <input type="Reset">
+  
+
+</form>
+           
+        </body>
+</html>
+<?php
+include "../Control/results.php";
+?>
